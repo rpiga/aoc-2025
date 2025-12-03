183bc108455b07d962685eb6f7c6282015ff340b
Roberto Piga <rpiga.net@gmail.com>
2025-12-03 21:49:22 +0100

    upload day1

---
+++ day1/aoc-1-part1.py
@@ -0,0 +1,24 @@
+
+def main():
+    dial=50
+    password=0
+
+    print('Hello.')
+    input_file = open('input.txt', 'r')
+
+    for line in input_file:
+        item = line.rstrip()
+        turn = 1 if item[0] == 'R' else -1
+        value = turn * int(item[1:])
+
+        dial = (dial + value) % 100
+        if dial==0:
+            password+=1
+            print(f'>> {item}/ {turn}/ {value}/ {dial}/ {password}')
+
+if __name__ == '__main__':
+    """
+    https://adventofcode.com/2025/day/1
+    """
+    main()
+
---
+++ day1/aoc-1-part2.py
@@ -0,0 +1,36 @@
+
+def main():
+    dial=50
+    password=0
+
+    print('Hello.')
+    input_file = open('input.txt', 'r')
+
+    for line in input_file:
+        item = line.rstrip()
+        turn = 1 if item[0] == 'R' else -1
+        value = turn * int(item[1:])
+        # print(f'>>> {dial+value}')
+
+        temp=0
+        if abs(value) > 99:
+            temp = abs(value)//100
+            remainder=0+(abs(value)-(temp*100))
+            value=turn*remainder
+            # print(f'>>>>> {value}\t{temp}\t{remainder}')
+
+        if (dial == 0):
+            dial = (dial + value)%100
+            password+=temp
+        else:
+            dial = (dial + value)
+            if dial%100==0 or dial< 0 or dial > 99:
+                password+=1
+            password+=temp
+            dial%=100
+        # print(f'>> {item}\t\t{value}\t{dial}\t{password}')
+    print(f'Result: {password}')
+
+if __name__ == '__main__':
+    main()
+
