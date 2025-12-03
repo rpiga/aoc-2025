26b4b685c76404028ede86fda71bc7d70180e4cb
Roberto Piga <rpiga.net@gmail.com>
2025-12-03 17:53:55 +0100

    Update day2-part1.py

--- day2/day2-part1.py
+++ day2/day2-part1.py
@@ -1,14 +1,38 @@
+def factorize(n):
+    print('factorz.')
+    factors=[]
+    while n > 1:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                n //= i
+                factors.append(i)
+                break
+    return factors
+
 def main():
-    f = open('test.txt', 'rt')
+    f=open('test.txt', 'rt')
 
     list = f.read().split(',')
 
     for l in list:
-        item = l.split('-')
-        item_top = item[0]
-        item_bottom = item[1]
+        item = l.rstrip().split('-')
+        item_bottom = int(item[0])
+        item_top = int(item[1])
         
         print(f'>>> {item}\t{item_top}\t{item_bottom}')
+
+        item_length=0
+
+        for i in range(item_bottom, item_top+1):
+            if len(str(i))%2 != 0:
+                print(f'{i}: pass')
+            else:
+                if len(str(i)) != item_length:
+                    item_length=len(str(i))
+                    fact=factorize(item_length)
+                
+                # if item_length%2 == 0:
+                print(f'{i}: {fact}')
 
     f.close()
 
