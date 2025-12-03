91958ea297961bf8c3c5823a47101f7c3ce8052b
Roberto Piga <rpiga.net@gmail.com>
2025-12-04 00:35:31 +0100

    update

--- day2/day2-part2.py
+++ day2/day2-part2.py
@@ -1,5 +1,15 @@
+def factorize(n):
+    factors=[]
+    while n > 1:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                n //= i
+                factors.append(i)
+                break
+    return set(factors)
+
 def main():
-    f=open('test.txt', 'rt')
+    f=open('test2.txt', 'rt')
 
     list = f.read().split(',')
 
@@ -9,22 +19,52 @@ def main():
         item_bottom = int(item[0])
         item_top = int(item[1])
         
-        # print(f'>>> {item}\t{item_top}\t{item_bottom}')
-
         for i in range(item_bottom, item_top+1):
-            if len(str(i))%2 != 0:
-                pass
-                # print(f'{i}: pass')
-            else:
-                i_s = str(i)
-                item_left, item_right = i_s[:len(i_s)//2], i_s[len(i_s)//2:]
+            i_s = str(i)
 
-                if item_left == item_right:
-                    total+=i
-                    # print(f'{i}: {item_left} {item_right}')
+            # Verify if all digits are the same
+            i_list = [ int(n) for n in i_s ]
+            if len(set(i_list)) == 1:
+                print(i)
+                total+=i
+            elif len(i_s)  in [3, 7, 11, 13]:
+                pass
+            else:
+                fact = factorize(len(i_s))
+                for j in fact:
+                    slices=[ i_s[k:k+j] for k in range(0, len(i_s), j) ]
+
+                    print(f'>> {i}\t\t{j}\t\t{slices}')
+                    # if len(slices) > 1 and len(set(slices)) == 1:
+
+            # for j in [i_list]:           
+            #     if len(set(j)) == 1:
+            #         total+=i
+            #         break
+            #     else:
+            #         continue
+            # print(f'>> {i_s}')
+
+                    # print(f'>> {i}\t\t{len(i_list)}:{j[0]}:{sum(i_list)}')
+                # elif len(j) in [3, 7, 11, 13]:
+                #     break
+                # else:
+                #     fact = factorize(len(j))
+                #     # print(f'>> {i}\t\t{j}\t\t{fact}')
+                #     slices = []
+                #     for k in fact:
+                #         slices = [ str(i)[ia:ia+1] for ia in range(0, len(i_s), k) ]
+                #         print(f'{i}\t\t{k}\t\t{slices}')
+                    #     slices = []
+                    #     parts.append(j[k:k+fact])
+                    # print(f'>> {i}\t\t{parts}')
+                        
+
+
                     
         
     print(f'Total: {total}')
+
 
     f.close()
 
