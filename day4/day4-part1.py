242be25244e5512efa313906f3d90c115574e241
Roberto Piga <rpiga.net@gmail.com>
2025-12-11 15:16:35 +0100

    initial

---
+++ day4/day4-part1.py
@@ -0,0 +1,16 @@
+def main():
+    print("Hello, day 4.")
+    
+    f=open('test.txt', 'rt')
+
+
+    f.close()
+    
+    pass
+
+if __name__ == "__main__":
+    """
+    https://adventofcode.com/2025/day/4
+    Author: https://github.com/rpiga
+    """
+    main()
